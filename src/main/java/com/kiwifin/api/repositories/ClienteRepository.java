449083b9d1817a321c810cda4daaaff486048fc2
Juliano Isijara <julianoisijara@hotmail.com>
2024-01-15 21:49:05 -0300

    Criação repositório Cliente.

---
+++ src/main/java/com/kiwifin/api/repositories/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.kiwifin.api.repositories;
+
+import com.kiwifin.api.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByNomeContains(String nome);
+
+    Cliente findByCpfEquals(String cpf);
+
+}
