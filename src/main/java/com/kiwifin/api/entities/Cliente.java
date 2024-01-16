283b179117358789e04d47d2cf09d67021c247a7
Juliano Isijara <julianoisijara@hotmail.com>
2024-01-15 21:47:59 -0300

    Criação entidade Cliente.

---
+++ src/main/java/com/kiwifin/api/entities/Cliente.java
@@ -0,0 +1,152 @@
+package com.kiwifin.api.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="CLIENTE", schema = "KIWIFIN")
+public class Cliente {
+
+    private Long idCliente;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String senha;
+    private Date dataNascimento;
+    private String celular;
+    private String cep;
+    private String cidade;
+    private String endereco;
+    private String uf;
+    private String complemento;
+
+    public Cliente() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idCliente")
+    public Long getidCliente() {
+        return idCliente;
+    }
+
+    public void setidCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Column(name="NOME", nullable = false)
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Column(name="EMAIL", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name="CPF", nullable = false, length = 11)
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Column(name="SENHA", nullable = false)
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DATA_NASCIMENTO", length = 7, nullable = false)
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Column(name="CELULAR", nullable = false)
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    @Column(name="CEP", nullable = false)
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Column(name="CIDADE", nullable = false)
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Column(name="ENDERECO", nullable = false)
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Column(name="UF", nullable = false,  length = 2)
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Column(name="COMPLEMENTO", nullable = false)
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return idCliente.equals(cliente.idCliente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCliente);
+    }
+}
