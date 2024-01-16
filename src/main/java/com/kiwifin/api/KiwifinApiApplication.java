83036ea87c97661b2f1489eb9dd6e04388ebe134
Juliano Isijara <julianoisijara@hotmail.com>
2024-01-15 21:29:45 -0300

    Iniciar.

---
+++ src/main/java/com/kiwifin/api/KiwifinApiApplication.java
@@ -0,0 +1,13 @@
+package com.kiwifin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwifinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwifinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiwifin/api/KiwifinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwifin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwifinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
