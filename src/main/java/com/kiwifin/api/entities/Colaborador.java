52de9ba4e7d8d7179d307f27793388dca5523b9b
Juliano Isijara <julianoisijara@hotmail.com>
2024-01-18 20:45:57 -0300

    Criação classe abstrata Colaborador.

---
+++ src/main/java/com/kiwifin/api/entities/Colaborador.java
@@ -0,0 +1,98 @@
+package com.kiwifin.api.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class Colaborador {
+
+
+
+    protected Long idColaborador;
+    protected String nome;
+    protected String email;
+    protected String cpf;
+    protected String senha;
+    protected Departamento departamento;
+    protected String perfil;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID_COLABORADOR")
+    public Long getIdColaborador() {
+        return idColaborador;
+    }
+
+    public void setIdColaborador(Long idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+
+    @Column(name="NOME", nullable = false)
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Column(name="EMAIL", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name="CPF", nullable = false, length = 11)
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Column(name="SENHA", nullable = false)
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "ID_DEPARTAMENTO", referencedColumnName = "ID_DEPARTAMENTO")
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Column(name="PERFIL", nullable = false)
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Colaborador that = (Colaborador) o;
+        return idColaborador.equals(that.idColaborador);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idColaborador);
+    }
+}
