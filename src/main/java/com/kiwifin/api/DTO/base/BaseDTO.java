533f920be8de926565bad4604556626a2091333e
Juliano Isijara <julianoisijara@hotmail.com>
2024-02-11 16:47:17 -0300

    BaseDTO - Criação.

---
+++ src/main/java/com/kiwifin/api/DTO/base/BaseDTO.java
@@ -0,0 +1,14 @@
+package com.kiwifin.api.DTO.base;
+
+public abstract class BaseDTO {
+
+    private String msgServico;
+
+    public String getMsgServico() {
+        return msgServico;
+    }
+
+    public void setMsgServico(String msgServico) {
+        this.msgServico = msgServico;
+    }
+}
