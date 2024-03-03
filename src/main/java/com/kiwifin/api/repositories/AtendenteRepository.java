13a71243ba96551b5e04291135b30c1526c7f873
Juliano Isijara <julianoisijara@hotmail.com>
2024-03-03 16:37:56 -0300

    AtendenteRepository - Pesquisar por CPF

--- src/main/java/com/kiwifin/api/repositories/AtendenteRepository.java
+++ src/main/java/com/kiwifin/api/repositories/AtendenteRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AtendenteRepository extends JpaRepository<Atendente, Long>{
+
+    Atendente findByCpfEquals(String cpf);
 }
