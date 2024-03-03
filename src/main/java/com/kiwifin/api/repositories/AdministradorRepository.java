ef9b3faf9d248c1040310cbd7e531ef9cd3ee8da
Juliano Isijara <julianoisijara@hotmail.com>
2024-03-03 16:38:12 -0300

    AdministradorRepository - Pesquisar por CPF

--- src/main/java/com/kiwifin/api/repositories/AdministradorRepository.java
+++ src/main/java/com/kiwifin/api/repositories/AdministradorRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
+
+    Administrador findByCpfEquals(String cpf);
 }
